06e7054f490d9225ee56aab6438f746f155e57a7
Mesiky <924256520@qq.com>
2016-09-03 13:56:32 +0800

    修改连续轮播混乱

--- 无缝轮播图/index.js
+++ 无缝轮播图/index.js
@@ -4,23 +4,24 @@ $(function(){
 	var oli=img.find('li');
 	var flag=true;//设置flag，防止用户过快点击按钮，造成动画的累积
 	var timer=null;
-	img.css('left','-490px');//设置默认打开的图片
 	//右键点击
 	$('#box').find('.btn_r').click(function(){
 		Lrun();//因为右键点击和自动滚动动作是相同的，所有可以将其封装
 	})
 	//左键点击
 	$('#box').find('.btn_l').click(function(){
-		num--;
+
 		if(flag){//判断flag是否为true
+			num--;//num也要放在if里面，因为放在外面只要点击按钮就会执行，但是动画不一定执行。
 			flag=false;//将flag设置为flase，表示用户点击了按钮，并且当前动画还没执行完
 			img.animate({
-				left:-490*num
+				left:num*-490+'px'
 			},function(){	
 				if(num<=0){
-					num=oli.length-2;
+					num=oli.length-2
 					img.css('left','-2450px');
 				}
+						console.log(num)
 				flag=true;//动画执行完之后，flag=true这个时候点击按钮才有效
 			})		
 		}
@@ -31,13 +32,13 @@ $(function(){
 	},2000)
 //向左滚动
 function Lrun(){
-	num++;
 	if(flag){
+		num++;//num也要放在if里面，因为放在外面只要点击按钮就会执行，但是动画不一定执行。
 		flag=false;
 		img.animate({
-			left:-490*num
+			left:num*-490+'px',
 		},function(){	
-			if(num>=oli.length-1){
+			if(num>oli.length-2){
 				num=1;
 				img.css('left','-490px');
 			}
